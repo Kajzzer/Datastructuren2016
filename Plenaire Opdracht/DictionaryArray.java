d81be038e605b47c5d6cd636c316ca9bb81d7912
Kaj Meijer <k.d.meijer17@gmail.com>
2016-02-12 14:47:16 +0100

    arrays zijn gemaakt, nu nog comparen en de tijd geven

--- Plenaire Opdracht/DictionaryArray.java
+++ Plenaire Opdracht/DictionaryArray.java
@@ -7,28 +7,48 @@ public class DictionaryArray {
 
 	public static void main(String[] args) {
 
-		try (BufferedReader br = new BufferedReader(new FileReader("wordlist.txt")))
+		DictionaryArray da = new DictionaryArray();
+		String dict[] = da.read("wordlist.txt");
+		String sample[] = da.read("sample_0OXg@T=T55.txt");
+		// for(int i=0; i < dict.length; i++){
+		// 	System.out.println(dict[i]);
+		// }
+		da.checkWords(dict,sample);
+
+	}	
+
+	public String[] read(String file){
+
+		int count = 0;
+		String control;
+		String dict[] = new String[1];
+
+
+		try (BufferedReader br = new BufferedReader(new FileReader(file)))
 		{
 
-			String control;
-			
-			int numberOfWords = 0;
-
 			while ((control = br.readLine()) != null) {
-				numberOfWords += 1;
+				if(count == (dict.length-1)){
+					dict = Arrays.copyOf(dict, dict.length*2);
+				}
+				dict[count] = control;
+				count += 1;
 			}
-
-			String words[] = new String[numberOfWords];
-
-			for(int i = 0; i < numberOfWords; i++){
-				words[i] = br.readLine();
-			}
-			System.out.println(words[2]);
-			System.out.println(numberOfWords);
-
 		} catch (IOException e) {
 			e.printStackTrace();
-		} 
+		}
+		dict = Arrays.copyOf(dict, count);
+		return dict; 
+	}
 
+	public checkWords(String dict[], String sample[]){
+
+		for(int i=0; i < sample.length; i++){
+			for(int j=0; j < dict.length; j++){
+				
+			}
+		}
 	}
 }
+
+// SimonsHashTable
